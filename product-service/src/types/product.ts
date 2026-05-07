0f594b4fe6447dd1ee895e4f33d9fcb2c7317dd7
Fedor Kaputsky <fedor.kaputsky@sap.com>
2026-05-07 23:03:57 +0200

    feat: add new TS models

--- product-service/src/types/product.ts
+++ product-service/src/types/product.ts
@@ -3,5 +3,13 @@ export interface Product {
   title: string;
   description: string;
   price: number;
+}
+
+export interface Stock {
+  product_id: string;
+  count: number;
+}
+
+export interface ProductWithStock extends Product {
   count: number;
 }
